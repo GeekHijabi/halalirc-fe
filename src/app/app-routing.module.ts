9c97ce4540f31a2f5689ad4b42865aa6b19e4d8b
geekhijabi <bimpeadetunji@rocketmail.com>
2018-05-20 19:54:06 +0100

    feature(signin-signup): Implement signin and signup functionality
    Currently, a user cannot signin or signup
    to the app as no api call is being
    made.
    
    This commit implements the feature that
    allows a user signup and signin to the
    app. It also redirects the user to the
    required route
    
    [Finishes #157353458]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+    {path: '*', component: AppComponent},
     { path: 'signup', component: SignupComponent },
-    { path: 'signin', component: SigninComponent }
+    { path: 'signin', component: SigninComponent },
+    { path: 'companies', component: CompaniesComponent }
 ];
 
 @NgModule({
